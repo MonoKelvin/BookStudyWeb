0b2dc8561b9d2c0fac8102ebce34c353e1b95563
MonoKelvin <15007083506@qq.com>
2019-10-30 12:20:07 +0800

    新增大量书籍，优化书籍提交和增添时的数据验证

--- book/test_insert_books.php
+++ book/test_insert_books.php
@@ -1,18 +1,86 @@
 <?php
 
-// require_once('../api/BookAPI.php');
+require_once(dirname(__FILE__) . '\..\api\mysql_api.php');
+require_once(dirname(__FILE__) . '\..\api\utility.php');
 
-// $res = MySqlAPI::getInstance()->useDataBase('bookdb')->getRow("select COUNT(*) from `bookinfo`");
-// var_dump($res);
-// $res = MySqlAPI::getInstance()->getRow("select COUNT(*) from `bookdetail`");
-// var_dump($res);
+$booksFile = dirname(__FILE__) . '\books.txt';
+$fp = fopen($booksFile, 'r');
+$booksArray = fread($fp, filesize($booksFile));
+fclose($fp);
+$booksArray = explode(',', $booksArray);
 
-// $url = 'https://api.douban.com/v2/book/isbn/9787507208740?apikey=0df993c66c0c636e29ecbb5344252a4a';
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// $response = curl_exec($ch);
-// curl_close($ch);
-// storeBookFromDouBan($response);
+// 打开数据库
+$db = MySqlAPI::getInstance();
+$book_ids = $db->getAll('select id from bookinfo');
+$ids = [];
+foreach ($book_ids as $val) {
+    $ids[$val['id']] = $val['id'];
+}
+
+foreach ($booksArray as $book) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $book);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $json_obj = json_decode($response, true);
+    $ret_code = @$json_obj['code'] ? $json_obj['code'] : 200;
+    if ($json_obj == null || $json_obj == false || $ret_code != 200) {
+        $db->close();
+        die('json_prase_is_failed');
+    }
+
+    if (isset($ids[$json_obj['id']])) {
+        continue;
+    }
+
+    $img_url = $json_obj['image'];
+    $img_file_name = $json_obj['id'] . substr($img_url, strripos($img_url, '.'));
+    $local_img_path = 'http://api.bookstudy.com/book/image/' . $img_file_name;
+
+    // 基本信息的获取
+    $author = implode(",", $json_obj['author']);
+    $translator = implode(",", $json_obj['translator']);
+    $pages = $json_obj['pages'];
+    if (!is_numeric($pages)) {
+        preg_match('/\d+/', $pages, $pages);
+        $pages = $pages[0];
+    }
+    $data = [
+        'id' => $json_obj['id'],
+        'title' => addslashes($json_obj['title']),
+        'author' => addslashes($author),
+        'publisher' => addslashes($json_obj['publisher']),
+        'pages' => $pages,
+        'pubdate' => $json_obj['pubdate'],
+        'rating' => $json_obj['rating']['average'],
+        'image' => $local_img_path,
+        'summary' => $json_obj['summary'],
+        'translator' => addslashes($translator)
+    ];
+    $res = $db->insert('bookinfo', $data);
+
+    // 详细信息的获取
+    $tags = '';
+    foreach ($json_obj['tags'] as $val) {
+        $tags .= $val['name'] . ',';
+    }
+    $tags = trim($tags, ',');
+    $data = [
+        'id' => $json_obj['id'],
+        'isbn13' => $json_obj['isbn13'],
+        'subtitle' => addslashes($json_obj['subtitle']),
+        'origin_title' => addslashes($json_obj['origin_title']),
+        'binding' => $json_obj['binding'],
+        'tags' => addslashes($tags),
+        'author_intro' => addslashes($json_obj['author_intro']),
+        'catalog' => addslashes($json_obj['catalog']),
+        'price' => $json_obj['price']
+    ];
+    $res = $db->insert('bookdetail', $data);
+}
+
+$db->close();
